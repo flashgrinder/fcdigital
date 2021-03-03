9816febf182f574e4e5a7d67599e0cb93a3c695b
flashgrinder <flashgrinder92@gmail.com>
2021-03-03 13:07:54 +0400

    fix js 030321

--- src/libs/modules/page-transition.js
+++ src/libs/modules/page-transition.js
@@ -39,6 +39,7 @@ function pageTransition() {
     });
 }
 
+// CONTENT ANIMATION
 function contentAnimation() {
 
     let tl = gsap.timeline();
--- src/libs/scripts.js
+++ src/libs/scripts.js
@@ -1,10 +1,8 @@
-// import marquee from '../libs/modules/Marquee3k.js';
 import burger from '../libs/modules/burger-menu.js';
 import pageTrs from '../libs/modules/page-transition.js';
 
 document.addEventListener('DOMContentLoaded', function(e) {
     
     burger.init();
-    // marquee.init();
 
 });
